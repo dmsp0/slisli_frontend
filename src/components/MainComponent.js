a95d5ec02099797d1f106f56b54e40517b915c8d
eunhye <dmsp2377@naver.com>
2024-06-05 15:22:59 +0900

    detailforum add

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,9 @@
 import React from "react";
-import BasicMenu from "../components/menus/BasicMenu";
-import TopMenu from "../components/menus/TopMenu";
 import MainView from "../components/menus/MainView";
 import CategoryMenu from "../components/menus/CategoryMenu";
 import Trendbooth from "../components/menus/Trendbooth";
 import Deadlinebooth from "../components/menus/Deadlinebooth";
-import InfoPage from "../components/menus/InfoPage";
-// import InfoComponent from "./InfoComponent";
+
 
 
 function MainComponent(){
@@ -19,8 +16,7 @@ function MainComponent(){
                 <CategoryMenu />
                 <Trendbooth />
                 <Deadlinebooth />
-                <InfoPage />
-                {/* <InfoComponent /> */}
+
                 
             </div>
             
