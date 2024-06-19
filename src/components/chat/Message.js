a249a781c924ba939a5af57dc020c5986d783176
eunhye <dmsp2377@naver.com>
2024-06-19 12:33:03 +0900

    modify boothDetail.js iw , chat eunhye

--- src/components/chat/Message.js
+++ src/components/chat/Message.js
@@ -1,14 +1,14 @@
-import React from 'react';
 import './Message.css';
 
-const Message = ({ userId, message, time, name }) => {
-  const isSentByMe = userId === name;
+const Message = ({ userId, message, time, nickname }) => {
+  const myName = localStorage.getItem('name');
+  const isSentByMe = nickname === myName; // 수정된 부분: userId 대신 nickname으로 비교
 
   return (
     <div className={`message ${isSentByMe ? 'sent' : 'received'}`}>
       {!isSentByMe && (
         <span className="profile">
-          <span className="user">{name}</span>
+          <span className="user">{nickname}</span>
         </span>
       )}
       <span className="message-text">{message}</span>
